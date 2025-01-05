7c8d981aafae35c6a6779e1a0d8e286b22c4a3b6
IYXYI <55164519+IYXYI@users.noreply.github.com>
2025-01-05 18:09:10 +0100

    Create models.py

---
+++ models.py
@@ -0,0 +1,19 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(20), default='Pending')
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
